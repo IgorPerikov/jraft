a745d2659eed773189124981176669aad4b964c6
IgorPerikov <perikov.igor@gmail.com>
2017-12-10 21:27:50 +0300

    move all output to stderr

--- src/main/java/com/github/igorperikov/jraft/config/AppConfiguration.java
+++ src/main/java/com/github/igorperikov/jraft/config/AppConfiguration.java
@@ -2,17 +2,18 @@ package com.github.igorperikov.jraft.config;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.github.igorperikov.jraft.Node;
-import com.github.igorperikov.jraft.message.*;
+import com.github.igorperikov.jraft.message.MessageDispatcher;
+import com.github.igorperikov.jraft.message.MessageValidator;
+import com.github.igorperikov.jraft.message.MessageWriter;
+import com.github.igorperikov.jraft.message.MessagesReader;
+import com.github.igorperikov.jraft.message.handler.RaftInitHandler;
 import com.github.igorperikov.jraft.persistent.FileBasedPersistentService;
 import com.github.igorperikov.jraft.persistent.PersistentService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Profile;
 
 @Configuration
-@Profile("maelstrom")
 public class AppConfiguration {
-
     @Bean
     public ObjectMapper objectMapper() {
         return new ObjectMapper();
